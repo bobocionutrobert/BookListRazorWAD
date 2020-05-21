581e9b684f55d67cc7345f10dcbbd11da662ef65
bobocionutrobert <33292001+bobocionutrobert@users.noreply.github.com>
2020-05-21 13:33:36 +0300

    Initial commit

---
+++ BookListRazor/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using BookListRazor.Areas.Identity.Data;
+using BookListRazor.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.CodeAnalysis.Options;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BookListRazor.Areas.Identity.IdentityHostingStartup))]
+namespace BookListRazor.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<BookListRazorContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("BookListRazorContextConnection")));
+
+                services.AddDefaultIdentity<BookListRazorUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                })
+                    .AddRoles<IdentityRole>()
+                    .AddEntityFrameworkStores<BookListRazorContext>();
+            });
+        }
+    }
+}
---
+++ BookListRazor/Controllers/BookController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Data;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Controllers
+{
+    [Route("api/Book")]
+    [ApiController]
+    [Authorize]
+    public class BookController : Controller
+    {
+        private readonly BookListRazorContext _db;
+
+        public BookController(BookListRazorContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data =await _db.Book.ToListAsync() });
+        }
+
+        [HttpDelete]
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb = await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+            if (bookFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Deleting" });
+            }
+            _db.Book.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete successful" });
+        }
+    }
+}
